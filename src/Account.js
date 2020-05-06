48ceb4f639ab4c1fbaba0e5f56cefe51a4266b26
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-05-05 18:15:19 -0700

    checking and savings functionalities done

--- src/Account.js
+++ src/Account.js
@@ -3,16 +3,44 @@ import React, { useState } from 'react'
 const Account = props => {
     let [amount, setAmount] = useState(0)
     let [balance, setBalance] = useState(0)
+    let [message, setMessage] = useState('')
+    let [trans, setTypeOfTrans] = useState('')
+    const MSG_NEGATIVE_NUMBER = "Number should be greater than zero"
+    const MSG_NAN = "Please enter a number"
+    const MSG_WITHDRAW = "Amount for withdrawal exceeds the balance"
+ 
+
 
     const handleSubmit = e => {
         e.preventDefault()
-        if (isNaN(amount)) {
-            console.log("Not a number")
+        setMessage('')
+        if (isNaN(amount) ) {
+            setMessage(MSG_NAN)
         }
-        else {
-            setBalance(balance + Number(amount))
+        else if(Number(amount)<0) {
+            setMessage(MSG_NEGATIVE_NUMBER)
+            
+        }
+        else if(Number(amount)>0){
+            if(trans === "Deposit"){
+                setBalance(balance + Number(amount))
+            }
+            else if(trans === "Withdraw"){
+                withdraw()
+            }
         }
         setAmount(0)
+        
+    } 
+
+
+    const withdraw = ()=>{
+            if(balance >= Number(amount)){
+                setBalance(balance - Number(amount))
+            }
+            else{
+                setMessage(MSG_WITHDRAW)
+            }
     }
 
     let balanceClass = 'balance'
@@ -24,16 +52,20 @@ const Account = props => {
         <div className="account">
             <h2>{props.name}</h2>
             <div className={balanceClass}>${balance}</div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}> 
                 <input 
                     type="text" 
                     placeholder="enter an amount" 
                     value={amount} 
                     onChange={ e => setAmount(e.target.value) }
                 />
-                <input type="submit" value="Deposit" />
+                 <input type="submit" value="Deposit" onClick={(e)=>setTypeOfTrans(e.target.value)}/>
+                 <input type="submit" value="Withdraw" onClick={(e)=>setTypeOfTrans(e.target.value)}/> 
+             
             </form>
+            <div> {message}   </div>
         </div>
+     
     )
 }
 
